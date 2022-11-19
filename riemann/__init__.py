1948ffe7fb0d9023e22b81c24de48350ccb96191
Jacob Lee <JLpython@outlook.com>
2022-11-19 14:58:53 -0800

    doc: update docstrings

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -1,4 +1,31 @@
-"""
+r"""
+Computes the Riemann summation of a function in :math:`n`-dimensional space.
+
+From Wikipedia:
+
+    Let :math:`f: [a,b] \rightarrow \mathbb{R}` be a function defined on a closed interval
+    :math:`[a,b]` of the real numbers, :math:`\mathbb(R)`, and
+
+        .. math::
+
+            P = \{[x_{0},x_{1}], [x_{1},x_{2}], \dots , [x_{n-1},x_{n}]\},
+
+    be a partition of :math:`I`, where
+
+        .. math::
+
+            a = x_{0} < x_{1} < x_{2} < \dots < x_{n} = b.
+
+    A Riemann sum of :math:`S` of :math:`f` over :math:`I` with partition :math:`P` is defined as
+
+        .. math::
+
+            S = \sum_{i=1}^{n} f(x_{i}^{*}) \Delta x_{i}
+
+    where :math:`\Delta x_{i} = x_{i} - x_{i-1}` and :math:`x_{i}^{*} \in [x_{i-1},x_{i}]`.
+
+(`Source <https://en.wikipedia.org/wiki/Riemann_sum#Definition>`_)
+
 .. :py:class:: Dim
 
     :canonical: riemann._summation.Dimension
--- riemann/summation.py
+++ riemann/summation.py
@@ -1,40 +1,4 @@
 r"""
-Computes the Riemann summation of a function in :math:`n`-dimensional space.
-
-From Wikipedia:
-
-Let :math:`f: [a,b] \rightarrow \mathbb{R}` be a function defined on a closed interval
-:math:`[a,b]` of the real numbers, :math:`\mathbb(R)`, and
-
-.. math::
-
-    P = \{[x_{0},x_{1}], [x_{1},x_{2}], \dots , [x_{n-1},x_{n}]\},
-
-be a partition of :math:`I`, where
-
-.. math::
-
-    a = x_{0} < x_{1} < x_{2} < \dots < x_{n} = b.
-
-A Riemann sum of :math:`S` of :math:`f` over :math:`I` with partition :math:`P` is defined as
-
-.. math::
-
-    S = \sum_{i=1}^{n} f(x_{i}^{*}) \Delta x_{i}
-
-where :math:`\Delta x_{i} = x_{i} - x{i-1}` and :math:`x_{i}^{*} \in [x_{i-1},x_{i}]`.
-
-(`Source <https://en.wikipedia.org/wiki/Riemann_sum#Definition>`_)
-
-From Wikipedia:
-
-Specific choices of :math:`x_{i}^{*}` gives us different types of Riemann sums:
-
-- If :math:`x_{i}^{*} = x_{i-1}` for all :math:`i`, then :math:`S` is called a **left rule** or **left Riemann sum**.
-- If :math:`x_{i}^{*} = x_{i}` for all :math:`i`, then :math:`S` is called a **right rule** or **right Riemann sum**.
-- If :math:`x_{i}^{*} = \frac{x_{i} + x_{i-1}}{2}` for all :math:`i`, then :math:`S` is called the **midpoint rule** or **middle Riemann sum**.
-
-(`Source <https://en.wikipedia.org/wiki/Riemann_sum#Some_specific_types_of_Riemann_sums>`_)
 
 .. py:class:: D
 
