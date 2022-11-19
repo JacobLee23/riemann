54131d15ff205f9591c0be47e7cd181ee7aea348
Jacob Lee <JLpython@outlook.com>
2022-11-19 12:16:14 -0800

    feat: implement n-dimensional riemann summation

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -1,3 +1,13 @@
 """
 
 """
+
+from ._summation import LOWER, MIDPOINT, UPPER
+from ._summation import Dimension
+from ._summation import summation
+
+
+L, M, U = LOWER, MIDPOINT, UPPER
+
+
+Dim = Dimension
