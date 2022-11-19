cf8cbf55b31edce6522396c143502439cc6d1d75
Jacob Lee <JLpython@outlook.com>
2022-11-19 15:03:28 -0800

    doc: update docstrings

--- riemann/__init__.py
+++ riemann/__init__.py
@@ -26,6 +26,20 @@ From Wikipedia:
 
 (`Source <https://en.wikipedia.org/wiki/Riemann_sum#Definition>`_)
 
+From Wikipedia:
+
+    Higher dimensional Riemann sums follow a similar pattern as from one to two to three dimensions.
+    For an arbitrary dimension, :math:`n`, a Riemann sum can be written as
+
+        .. math::
+
+            S = \sum_{i=1}^{n} f(P_{i}^{*}) \Delta V_{i}
+
+    where :math:`P_{i}^{*} \in V_{i}`, that is, it's a point in the :math:`n`-dimensional cell
+    :math:`V_{i}` with :math:`n`-dimensional volume :math:`\Delta V_{i}`.
+
+(`Source <https://en.wikipedia.org/wiki/Riemann_sum#Arbitrary_number_of_dimensions>`_)
+
 .. :py:class:: Dim
 
     :canonical: riemann._summation.Dimension
